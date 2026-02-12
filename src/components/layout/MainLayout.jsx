2287b6dcfbe18d7147fc6f24b4036f1e33908916
Ahmed Moneim <ahnedmoneimhacher26@gmail.com>
2026-02-13 01:03:28 +0200

    Fix: Unified navbar casing

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import MyNavbar from '../navBar/Navbar'
+import MyNavbar from '../navbar/Navbar'
 export default function MainLayout() {
   return (
     <>
